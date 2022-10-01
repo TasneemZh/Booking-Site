7a9837b569461372ac4b9780e0f47a753befd1b9
Tasneem Zahdeh <tasneemzh.1998@gmail.com>
2022-10-01 14:01:46 +0300

    Add login implementation

---
+++ src/main/java/pages/SignIn.java
@@ -0,0 +1,38 @@
+package pages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SignIn {
+	WebDriver driver;
+
+	public SignIn(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void getSepecificButton(String comparisonTxt) throws InterruptedException {
+		Thread.sleep(3000);
+		List<WebElement> buttonElements = this.driver.findElements(By.xpath("//button"));
+		for (WebElement element : buttonElements) {
+			if (element.getText().equals(comparisonTxt)) {
+				element.click();
+				Thread.sleep(3000);
+			}
+		}
+	}
+
+	public void loginIntoFigma(String email, String password) throws InterruptedException {
+		Thread.sleep(3000);
+		this.driver.switchTo().frame(this.driver.findElement(By.xpath("//iframe[@title='Auth']")));
+		WebElement emailField = this.driver.findElement(By.id("email"));
+		emailField.sendKeys(email);
+
+		WebElement passwordField = this.driver.findElement(By.id("current-password"));
+		passwordField.sendKeys(password);
+
+		getSepecificButton("Log in");
+	}
+}
