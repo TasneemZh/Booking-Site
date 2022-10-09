01eca840963d1441e56faa06d3bdbd761db1f7e3
Tasneem Zahdeh <tasneemzh.1998@gmail.com>
2022-10-09 17:36:13 +0300

    Complete project

--- src/main/java/browsers/OpenBrowser.java
+++ src/main/java/browsers/OpenBrowser.java
@@ -5,56 +5,39 @@ import java.util.HashMap;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 
 public class OpenBrowser {
+	final static boolean isHeadless = true;
+
 	public static WebDriver setChromeOptions() {
 		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
 		chromePrefs.put("profile.default_content_settings.popups", 0);
 
 		ChromeOptions options = new ChromeOptions();
 		options.setExperimentalOption("prefs", chromePrefs);
-		options.addArguments("--start-maximized");
-		options.addArguments("--headless");
 		options.addArguments("--disable-infobars");
-		options.addArguments("--window-size=1920,1080");
 		options.addArguments("--ignore-certificate-errors");
 		options.addArguments("--allow-running-insecure-content");
 
-		WebDriver webDriver = new ChromeDriver(options);
-		return webDriver;
-	}
+		if (isHeadless) {
+			options.addArguments("--headless");
+			options.addArguments("--window-size=1920,1080");
+		} else {
+			options.addArguments("--start-maximized");
+		}
 
-	public static WebDriver setFirefoxProfile() {
-		FirefoxProfile profile = new FirefoxProfile();
-
-		profile.setPreference("browser.helperApps.neverAsk.saveToDisk",
-				"text/csv,application/java-archive, application/x-msexcel,application/excel,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml,application/vnd.microsoft.portable-executable");
-
-		FirefoxOptions option = new FirefoxOptions();
-		option.setProfile(profile);
-
-		WebDriver webDriver = new FirefoxDriver(option);
-		return webDriver;
+		WebDriver driver = new ChromeDriver(options);
+		return driver;
 	}
 
 	public WebDriver createDriver(String browser) {
-		WebDriver webDriver;
+		WebDriver driver;
 		if (browser.equals("chrome")) {
 			System.setProperty("webdriver.chrome.driver", "./src/main/resources/drivers/chromedriver.exe");
-			webDriver = OpenBrowser.setChromeOptions();
-		} else if (browser.equals("edge")) {
-			System.setProperty("webdriver.edge.driver", "./src/main/resources/drivers/msedgedriver.exe");
-			webDriver = new EdgeDriver();
-		} else if (browser.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", "./src/main/resources/drivers/geckodriver.exe");
-			webDriver = OpenBrowser.setFirefoxProfile();
+			driver = OpenBrowser.setChromeOptions();
 		} else {
 			return null;
 		}
-		return webDriver;
+		return driver;
 	}
 }
